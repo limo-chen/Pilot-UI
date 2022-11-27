0e26dc52d05f775f05a628e412666ef405dfcda0
Calista <2320035651@qq.com>
2022-11-27 12:04:12 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>hi</div>
+<Limo/>
+ 
+</template>
+
+<script>
+import Limo from './components/Limo.vue'
+
+export default {
+  name: 'App',
+  components: {
+   Limo//这里做映射，缩写成一个
+  }
+}
+</script>
