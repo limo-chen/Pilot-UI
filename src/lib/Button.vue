f0acd9cdabee99ea61a28fcf908bbc4fbdc6a973
Calista <2320035651@qq.com>
2022-11-28 22:28:34 +0800

    添加theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,22 @@
-<template >
-    <button>
+<template >    
+        <button class="gulu-button"
+        :class="{[`theme-${theme}`]: theme}">
    <slot />
     </button>
 </template>
+<script lang="ts">
+export default {
+  props:{
+    theme:{
+        type:String,
+        default: 'button'
+    },
+  }
+}
+</script>
+
+<style lang="ts" scoped>
+ div {
+   border: 1px solid red;
+}
+</style>
