1621c5a5b5288a13050c590b3cc396cee8a1a1c2
Calista <2320035651@qq.com>
2022-11-29 14:33:30 +0800

    button样式没显示

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,26 +1,57 @@
 <template >    
-    <button class="gulu-button" :class="{[`theme-${theme}`]: theme}">
-   <slot />
+    <button class="polit-button" :class="classes" :disabled="disabled">
+      <div v-if="loading" class="polit-loadingIndicator"></div>
+      <slot />
     </button>
 </template>
 <script lang="ts">
+import { computed } from "vue";
 export default {
   props:{
     theme:{
         type:String,
         default: "button",
     },
+    size:{
+      type:String,
+      default:'button',
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled:{ 
+      type: Boolean,
+      default:false,
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props){
+    const {theme,size,level} = props;
+    const classes = computed(() => {
+  return {
+  [`polit-theme-${theme}`]: theme,
+  [`polit-theme-${size}`]: size,
+  [`poilt-level-${level}`]: level,
+};
+})
+return { classes };
   }
 }
 </script>
 
-<style lang="ts">
+<style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
-.gulu-button {
+$red: red;
+$grey: grey;
+.poilt-button {
   box-sizing: border-box;
   height: $h;
   padding: 0 12px;
@@ -34,6 +65,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -48,5 +80,111 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+
+&.polit-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.polit-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.polit-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.polit-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.polit-theme-button {
+    &.polit-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.polit-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.polit-theme-link {
+    &.polit-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.polit-theme-text {
+    &.polit-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.polit-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.polit-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.polit-theme-link, &.polit-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+  > .polit-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px; 
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: polit-spin 1s infinite linear;
+  }
 }
+@keyframes polit-spin {
+  0%{transform: rotate(0deg)} 
+  100%{transform: rotate(360deg)} 
+  }
 </style>
