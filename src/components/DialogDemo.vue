198358d9c1899de38d0200e4e76be6131968c934
Calista <2320035651@qq.com>
2022-11-30 15:06:11 +0800

    让Dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,9 @@
   <div>Dialog 示例</div>
       <h1>示例1</h1>
     <Button @click="toggle">toggle</Button>
-    <Dialog :visible="x"></Dialog>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" 
+    :ok="f1" :cancel="f2"
+    ></Dialog>
 </template>
 <script long="ts">
 import Dialog from '../lib/Dialog.vue'
@@ -17,7 +19,13 @@ export default {
       const toggle =()=>{
         x.value = !x.value
       }
-      return {x,toggle}
+      const f1 = ()=>{
+        return false
+      }
+      const f2 = ()=>{
+
+      }
+      return {x,toggle,f1,f2}
     }
 }
 </script>
