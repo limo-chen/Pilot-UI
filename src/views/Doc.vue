09a5b79b87af1bf241e294a5e3664a80f89703fc
Calista <2320035651@qq.com>
2022-12-03 01:30:51 +0800

    更新切换按钮的样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout">
-    <Topnav class="nav" />
+    <Topnav toggleMenuButtonVisible="true" class="nav" />
 
     <div class="content">
       <aside v-if="menuVisible">
