a25b5def9b5fa657ab1d4695abdf259f2812e1d7
Calista <2320035651@qq.com>
2022-12-02 15:43:02 +0800

    实现下划线滑动

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
        tabs示例
     </div>
     <h1>示例1</h1>
-    <Tabs>
+    <Tabs v-model:selected="x">
         <Tab title="导航1">内容一</Tab>
         <Tab title="导航2">内容二</Tab>
 
@@ -12,11 +12,19 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue'
 import Tab from '../lib/Tab.vue'
-
+import {
+  ref
+} from 'vue'
 export default {
   components :{
     Tabs,
     Tab
+  },
+  setup() {
+    const x = ref('导航1')
+    return {
+      x
+    }
   }  
 }
 </script>
